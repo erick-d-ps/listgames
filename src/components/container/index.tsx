29a48d45ceaf324f083af117b3bc2b0f381c6769
deverick <dncellassistec@gmail.com>
2025-06-25 16:25:57 -0300

    estilo medida global

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 
 export function Container({ children }: { children: ReactNode }) {
+  
   return (
   <div className="max-w-screen-xl mx-auto px-3">
     { children }
