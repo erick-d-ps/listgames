b56a1fd406e9dae7f77da540a8948d764e2086fd
deverick <dncellassistec@gmail.com>
2025-06-29 18:10:49 -0300

    criado metadata dinâmico

--- src/app/game/[id]/page.tsx
+++ src/app/game/[id]/page.tsx
@@ -1,9 +1,49 @@
 import { Container } from "@/components/container";
-import { GameProps } from "@/utils/types/game";
+import { GameProps, PropeParams } from "@/utils/types/game";
 import Image from "next/image";
 import { redirect } from "next/navigation";
 import { Label } from "../components/label";
 import { GameCard } from "@/components/GameCard";
+import { Metadata } from "next";
+
+export async function generateMetadata({params}: PropeParams): Promise<Metadata>{
+  try {
+    const response: GameProps = await fetch(
+      `${process.env.NEXT_API_URL}/next-api/?api=game&id=${params.id}`,
+      {next: { revalidate: 60 }}
+    )
+    .then((res) => res.json())
+    .catch(() => {
+      return{
+        title: "ListGames - Descubra jogos incríveis para se divertir "
+      }
+    })
+
+    return{
+      title: response.title,
+      description: `${response.description.slice(0, 100)}...`,
+      openGraph: {
+        title: response.title,
+        images: [response.image_url]
+      },
+      robots:{
+        index: true,
+        follow: true,
+        nocache: true,
+        googleBot: {
+          index: true,
+          follow: true,
+          noimageindex: true,
+        }
+      }
+    }
+    
+  } catch (err) {
+    return{
+      title: "ListGames - Descubra jogos incríveis para se divertir "
+    }
+  }
+}
 
 async function getData(id: string) {
   try {
@@ -29,12 +69,8 @@ async function getGameSorted() {
   }
 }
 
-export default async function Detalhe({
-  params: { id },
-}: {
-  params: { id: string };
-}) {
-  const data: GameProps = await getData(id);
+export default async function Detalhe({params}: PropeParams) {
+  const data: GameProps = await getData(params.id);
   const gameSorted: GameProps = await getGameSorted();
 
   if (!data) {
