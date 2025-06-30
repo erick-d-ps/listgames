1168c81bf6aa4a34899b581841c4f57838162ef9
deverick <dncellassistec@gmail.com>
2025-06-30 16:54:24 -0300

    inicio da pagina perfil

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,34 @@
+import { Container } from "@/components/container";
+import Image from "next/image";
+import { FaShareAlt } from "react-icons/fa";
+
+export default function profile() {
+  return (
+    <main className="text-white">
+      <Container>
+        <section className="mt-8 mb-6 flex flex-col items-center justify-between relative gap-3 sm:flex-row">
+          <div className="w-full flex items-center gap-4 text-lg flex-col sm:flex-row justify-center sm:justify-normal">
+            <Image
+              src="/user.png"
+              alt="iamgem perfil do usuario"
+              className="rounded-full w-56 h-56 object-cover"
+              width={100}
+              height={100}
+              quality={100}
+              priority
+            />
+            <h1 className="font-bold text-2xl">Meus perfil</h1>
+          </div>
+          <div className="sm:absolute top-0 right-0 flex gap-3 items-center justify-center mt-2">
+            <button className="bg-gray-800 px-4 py-3 rounded-lg">
+              Configurações
+            </button>
+            <button className="bg-gray-800 px-4 py-3 rounded-lg">
+              <FaShareAlt size={24} color="#fff" />
+            </button>
+          </div>
+        </section>
+      </Container>
+    </main>
+  );
+}
